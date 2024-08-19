2db7b737f85bacd76017da28f7ee865ec3316efd
Eduardo Godoi da Silveira <eduardogodoisilveira@gmail.com>
2024-08-18 21:46:05 -0300

    Adding Customer entity.

---
+++ src/main/java/com/springbootcourse/Customer.java
@@ -0,0 +1,27 @@
+package com.springbootcourse;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+}
